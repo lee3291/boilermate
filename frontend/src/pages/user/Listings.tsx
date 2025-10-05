c612ad46e00a775ce05410de04280c79258107f0
Benjamin Lobos <blobosle@purdue.edu>
2025-10-04 23:42:35 -0400

    feat: rudimentary implementation of nav bar

--- frontend/src/pages/user/Listings.tsx
+++ frontend/src/pages/user/Listings.tsx
@@ -1,9 +1,12 @@
 export default function Listings() {
     return (
         <div className="bg-white w-full h-400">
-            <h1 className="text-2xl">
-                Hello
-            </h1>
+        <div className="flex justify-around pt-7">
+        <div className="relative w-full">
+        <div className="absolute top-1 left-1 h-16 w-12/13 rounded-2xl bg-black/80" />
+        <div className="relative z-10 h-16 w-12/13 rounded-2xl border bg-white" />
+        </div>
+        </div>
         </div>
     )
 }
