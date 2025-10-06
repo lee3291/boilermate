dc449b6a7054f040389e2002470bfbc00629e13f
Ethan <122648917+lee3291@users.noreply.github.com>
2025-10-06 10:57:42 -0400

    fix: correct a simple typo in css

--- frontend/src/pages/public/SignUpPage.tsx
+++ frontend/src/pages/public/SignUpPage.tsx
@@ -8,12 +8,12 @@ const SignUpPage = () => {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    console.log('Submitting with:', { email, password });
+    console.log('Submitting with:', { email, password }); // This will be replaced with backend code
   };
 
   return (
     <div className='flex items-center justify-center min-h screen bg-gray-100'>
-      <div className='p-8 bg-white rounded-lg shadow-md w-full max-w-wd'>
+      <div className='p-8 bg-white rounded-lg shadow-md w-full max-w-md'>
         <h1 className='text-2hl font-bold mb-6 text-center'>Create Your Account</h1>
         <form onSubmit={handleSubmit}></form>
         <InputField
