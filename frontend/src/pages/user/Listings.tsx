08de1c7e29332e47710b334956c99596d6ab696a
Benjamin Lobos <blobosle@purdue.edu>
2025-10-11 16:12:53 -0400

    feat: completed navegation interface

--- frontend/src/pages/user/Listings.tsx
+++ frontend/src/pages/user/Listings.tsx
@@ -1,12 +1,17 @@
+import Navbar from "./components/Navbar.tsx"
+
 export default function Listings() {
     return (
-        <div className="bg-white w-full h-400">
-        <div className="flex justify-around pt-7">
-        <div className="relative w-full">
-        <div className="absolute top-1 left-1 h-16 w-12/13 rounded-2xl bg-black/80" />
-        <div className="relative z-10 h-16 w-12/13 rounded-2xl border bg-white" />
+        <div className="bg-white w-full h-[400px]">
+            <Navbar />
+
+            <div className="grid grid-cols-2 gap-4">
+                <div className="pt-4 pl-15">
+                    <h1 className="pb-2 font-extralight font-serif text-[55px] text-maingray">Listings</h1>
+                    <button className="h-12 w-60 bg-white border-black border-1 rounded-[100px] font-sans">Create a Listing</button>
+                </div>
+            </div>
         </div>
-        </div>
-        </div>
-    )
+    );
 }
+
