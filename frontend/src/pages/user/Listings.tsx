09368f493825214ce1dc5566901ba54c9782a1ac
Benjamin Lobos <blobosle@purdue.edu>
2025-10-12 08:39:38 -0400

    feat: added more components to listings page

--- frontend/src/pages/user/Listings.tsx
+++ frontend/src/pages/user/Listings.tsx
@@ -5,11 +5,14 @@ export default function Listings() {
         <div className="bg-white w-full h-[400px]">
             <Navbar />
 
-            <div className="grid grid-cols-2 gap-4">
-                <div className="pt-4 pl-15">
-                    <h1 className="pb-2 font-extralight font-serif text-[55px] text-maingray">Listings</h1>
+            <div className="pt-4 flex items-start gap-4">
+                <div className="pl-15">
+                    <h1 className="pb-2 font-extralight font-sourceserif4-18pt-regular tracking-[-0.02em] text-[55px] text-maingray">Listings</h1>
                     <button className="h-12 w-60 bg-white border-black border-1 rounded-[100px] font-sans">Create a Listing</button>
                 </div>
+                <div aria-hidden className="mt-10 w-px bg-gray-900 h-200" />
+
+                <div className="flex-1">This is where content must be rendered</div>
             </div>
         </div>
     );
