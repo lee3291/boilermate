15eff05b348efc9865fb22c1fe27f3675d33346a
Benjamin Lobos <blobosle@purdue.edu>
2025-10-16 13:25:27 -0400

    fix: post merge sprint1-listings conflict resolution

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '@core/database/prisma.module';
 import { ListingsModule } from '@modules/listings/listings.module'
-import { DogsModule } from '@modules/dogs/dogs.module';
+// import { DogsModule } from '@modules/dogs/dogs.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { EmailVerificationModule } from './modules/email-verification/email-verification.module';
 
 @Module({
-  imports: [PrismaModule, ListingsModule, DogsModule, AuthModule, EmailVerificationModule],
+  imports: [PrismaModule, ListingsModule, AuthModule, EmailVerificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
