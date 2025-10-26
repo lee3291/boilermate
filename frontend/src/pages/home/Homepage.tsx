05d5887e3fb51191959e7c968fac3181ddfd1b1b
Benjamin Lobos <blobosle@purdue.edu>
2025-10-26 17:15:05 -0400

    feat: added interface for homepage

--- frontend/src/pages/home/Homepage.tsx
+++ frontend/src/pages/home/Homepage.tsx
@@ -1,7 +1,27 @@
+import HomeNavbar from "./components/HomeNavbar"
+
+import MainImage from "../../assets/images/beg.webp"
+
 export default function Homepage() {
     return (
-        <div className="h-200 bg-black">
+        <div className="h-400 bg-mainbrown">
+            <HomeNavbar />
 
+            <div className="grid grid-cols-2 pt-5 px-4 gap-5">
+                <img src={MainImage} className="w-full h-auto rounded-lg"/>
+                <div className="pt-10 flex flex-col justify-start origin-right items-end">
+                    <h1 className="font-sourceserif4-18pt-regular tracking-tighter scale-x-90 text-maingray origin-left text-[230px] leading-none">
+                        Boiler
+                    </h1>
+                    <h1 className="font-sourceserif4-18pt-regular tracking-tighter scale-x-90 text-maingray origin-left text-[230px] -mt-10 leading-none">
+                        Mate
+                    </h1>
+
+                    <h3 className="font-sourceserif4-18pt-light tracking-tighter scale-x-90 text-maingray text-[55px] leading-none text-right pt-10">
+                        Purdue’s Number 1 Roomate Matching Solution
+                    </h3>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ frontend/src/pages/home/components/HomeNavbar.tsx
@@ -0,0 +1,23 @@
+import accountIcon from '@/assets/images/account.png';
+
+export default function Navbar() {
+    return (
+        <div className='sticky top-0 z-50 w-full pt-3 px-3'>
+            <nav className='relative z-10 mx-auto h-16 w-full rounded-lg border bg-mainbrown px-4 md:px-6'>
+                <div className='flex h-full items-center justify-between gap-4'>
+                    <a
+                        className='cursor-pointer font-sourceserif4-18pt-light scale-x-90 text-[22px] tracking-tight select-none'
+                        href='/'
+                    >
+                        BoilerMate
+                    </a>
+
+                    <a href='/temp-account' className='flex items-center gap-2'>
+                        <img src={accountIcon} className='h-6 w-auto' />
+                    </a>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
