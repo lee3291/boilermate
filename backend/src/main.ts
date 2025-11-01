0419e052ad6420a1006fab65eea07600bc1e9f74
Ethan <122648917+lee3291@users.noreply.github.com>
2025-11-01 16:40:13 -0400

    fix: add PATCH under CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
 
